5a83e20853b79e334c958026811db88e6fd6d36b
Kamal Latif <razakam79@gmail.com>
2015-09-11 13:08:49 +0100

    Update app_config.php to web user

--- Connections/app_config.php
+++ Connections/app_config.php
@@ -3,8 +3,8 @@
 //Database connection constants
 
 define("DATABASE_HOST", "localhost");
-define("DATABASE_USERNAME", "root");
-define("DATABASE_PASSWORD", "worksucks");
+define("DATABASE_USERNAME", "web");
+define("DATABASE_PASSWORD", "Saturn3759");
 define("DATABASE_NAME", "teambook");
 
 ?>
